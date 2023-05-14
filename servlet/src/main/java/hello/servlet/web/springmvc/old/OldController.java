09689f239397040756e6c1a91d1db8474d36612d
soohyunnn <sksmsqodn20@gmail.com>
2023-05-14 23:31:05 +0900

    [스프링 MVC - 구조 이해] 뷰 리졸버

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -11,6 +11,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
+
 }
