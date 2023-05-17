1e551213650f2d46b4f89b6bc53f48bbd93d836f
soohyunnn <sksmsqodn20@gmail.com>
2023-05-17 10:36:24 +0900

    [스프링 MVC - 웹 페이지 만들기] 상품 도메인 개발

---
+++ item-service/src/main/java/hello/itemservice/domain/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();  // static
+    private static long sequence = 0L; // static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        // !! 이 경우는 ItemParamDto 를 만들어서 아래 3가지 항목만 넣어놓는 것이 맞는 설계이다.
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
---
+++ item-service/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -0,0 +1,70 @@
+package hello.itemservice.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();  // 테스트가 끝날때마다 초기화
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        
+        //when
+        Item saveItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(saveItem);
+    }
+
+
+    @Test
+    void findAll() {
+        //given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 10);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        //when
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+
+    @Test
+    void updateItem() {
+        //given
+        Item item = new Item("item1", 10000, 10);
+
+        Item saveItem = itemRepository.save(item);
+        Long itemId = saveItem.getId();
+
+        //when
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        //then
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+}
